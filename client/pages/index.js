b3a381d43881c5198e9c9dcc21d30bb6c0c29cfa
Valerio BIANCHI <valerio.bianchi@alten.it>
2023-07-17 17:43:18 +0200

    fixing get initial props

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,25 @@
 import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
-    return <h1>You have signed in! Welcome, { currentUser }</h1>
+    return <h1>You have signed in! Welcome, { currentUser.email }</h1>
 }
 
-LandingPage.getInitialProps = async () => {
-    const response = await axios.get('/api/users/currentuser');
-
-    return response.data;
+LandingPage.getInitialProps = async ({ req  }) => {
+    if(!window){
+        // we are on the server
+        const { data } = await axios.get(
+            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', 
+            {
+                headers: {
+                    Host: 'ticketing.dev'
+                }
+            }
+        );
+        return data;
+    } else {
+        const { data } = await axios.get('/api/users/currentuser');
+        return data;
+    }
 }
 
 export default LandingPage;
