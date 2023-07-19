c55b56fbbcc3968d21788f5727ce909985ff8900
Valerio BIANCHI <valerio.bianchi@alten.it>
2023-07-19 17:47:23 +0200

    signin form and build cient

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import buidClient from '../api/buid-client';
 
-export default ({ Component, pageProps}) => {
-    return <Component {...pageProps} />
+const AppComponent = ({ Component, pageProps, currentUser}) => {
+    return <React.Fragment>
+        <Header></Header>
+        <Component {...pageProps} />
+    </React.Fragment>
+     
 }
+
+AppComponent.getInitialProps = async (appContext) =>{
+    const { data } = await buidClient(appContext.ctx).get('/api/users/currentuser');
+    let pageProps = {}
+    if(appContext.Component.getInitialProps) {
+        pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    }
+    return {
+        pageProps,
+        ...data
+    };
+}
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,25 +1,12 @@
-import axios from 'axios';
+import buidClient from '../api/buid-client';
 
 const LandingPage = ({ currentUser }) => {
-    return <h1>You have signed in! Welcome, { currentUser.email }</h1>
+    return currentUser ? <h1>You have signed in! Welcome, { currentUser.email }</h1> : <h1>You are not signed in</h1>
 }
 
-LandingPage.getInitialProps = async ({ req  }) => {
-    if(!window){
-        // we are on the server
-        const { data } = await axios.get(
-            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', 
-            {
-                headers: {
-                    Host: 'ticketing.dev'
-                }
-            }
-        );
-        return data;
-    } else {
-        const { data } = await axios.get('/api/users/currentuser');
-        return data;
-    }
+LandingPage.getInitialProps = async (context) => {
+    const { data } = await buidClient(context).get('/api/users/currentuser');
+    return data;
 }
 
 export default LandingPage;
