9289c62ab0490a8a8fe295906aff5443b101c088
Valerio BIANCHI <valerio.bianchi@alten.it>
2023-07-20 16:39:51 +0200

    common errors and middlewares inside common module

---
+++ common/build/errors/request-validation-error.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.RequestValidationError = void 0;
+const custom_error_1 = require("./custom-error");
+class RequestValidationError extends custom_error_1.CustomError {
+    constructor(errors) {
+        super('Invalid request parameters');
+        this.errors = errors;
+        this.statusCode = 400;
+        // Only because we are extending a built in class
+        Object.setPrototypeOf(this, RequestValidationError.prototype);
+    }
+    serializeErrors() {
+        return this.errors.map(error => {
+            if (error.type === 'field') {
+                return { message: error.msg, field: error.path };
+            }
+            return { message: error.msg };
+        });
+    }
+}
+exports.RequestValidationError = RequestValidationError;
