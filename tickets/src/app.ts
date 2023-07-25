7cbf279c7fd85411c39526321e870f2d3670e2e1
Valerio BIANCHI <valerio.bianchi@alten.it>
2023-07-25 15:04:14 +0200

    index tickets api

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -7,6 +7,7 @@ import cookieSession from 'cookie-session';
 import { currentUser, errorHandler, NotFoundError } from '@sgtickets/common';
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes';
 
 const app = express();
 
@@ -24,6 +25,7 @@ app.use(currentUser);
 
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all('*', async (req, res) => {
     throw new NotFoundError();
