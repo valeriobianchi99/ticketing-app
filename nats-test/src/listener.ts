3a638d6b742003700be731d9bca068ce44f5b301
Valerio BIANCHI <valerio.bianchi@alten.it>
2023-08-30 10:49:35 +0200

    client id generation

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,10 +1,11 @@
 import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
 
 console.clear();
 
 const stan = nats.connect(
     'ticketing',
-    '123',
+    randomBytes(4).toString('hex'),
     { 
         url: 'http://localhost:4222'
     }
