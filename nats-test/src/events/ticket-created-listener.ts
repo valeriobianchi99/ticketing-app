774576fef3fce05a284c93e91069a63b059d159b
Valerio BIANCHI <valerio.bianchi@alten.it>
2023-09-01 12:27:41 +0200

    ticket-created interfaces

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,8 +1,10 @@
 import { Message } from "node-nats-streaming";
 import { Listener } from "./base-listener";
+import { TicketCreatedEvent } from "./ticket-created-events";
+import { Subjects } from "./subjects";
 
-export class TicketCreatedListener extends Listener {
-    subject = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+    readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
     queueGroupName = 'payments-service';
     onMessage(data: any, msg: Message) {
         console.log('Event data:', data);
