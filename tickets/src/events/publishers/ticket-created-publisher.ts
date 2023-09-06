686f1027beca3e8a90caac1d11033ca2e06d32d0
Valerio BIANCHI <valerio.bianchi@alten.it>
2023-09-06 18:14:46 +0200

    singleton nats wrapper

---
+++ tickets/src/events/publishers/ticket-created-publisher.ts
@@ -0,0 +1,6 @@
+import { Publisher, Subjects, TicketCreatedEvent } from "@sgtickets/common";
+
+export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
+    subject: Subjects.TicketCreated = Subjects.TicketCreated;
+    
+}
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,8 @@ import { requireAuth, validateRequest } from '@sgtickets/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -26,6 +28,13 @@ router.post(
             userId: req.currentUser!.id
         });
         await ticket.save();
+        new TicketCreatedPublisher(natsWrapper.client).publish({
+            id: ticket.id,
+            title: ticket.title,
+            price: ticket.price,
+            userId: ticket.userId,
+            version: 1
+        })
         res.status(201).send(ticket);
     }
 )
