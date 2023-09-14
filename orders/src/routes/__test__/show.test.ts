feacc04c5ce6f18b133f47a4e3311b34a2065279
Valerio BIANCHI <valerio.bianchi@alten.it>
2023-09-14 16:46:03 +0200

    order deleting and other tests

---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,46 @@
+import request from "supertest";
+import { Ticket } from "../../models/ticket";
+import { app } from "../../app";
+
+it("fetches the order", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(200);
+  expect(fetchedOrder.id).toEqual(order.id);
+});
+
+it("returns an error if user is not authorized to fetch order", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+  await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set("Cookie", global.signin())
+    .send()
+    .expect(401);
+});
--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,13 +1,26 @@
 import express, { Request, Response } from "express";
 import { Order } from "../models/order";
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from "@sgtickets/common";
 
 const router = express.Router();
 
-router.get("/api/orders/:orderId", async (req: Request, res: Response) => {
-  const orders = await Order.find({
-    userId: req.currentUser!.id,
-  }).populate("ticket");
-  res.send(orders);
-});
+router.get(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate("ticket");
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
