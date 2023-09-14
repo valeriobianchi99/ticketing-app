89c3f2dc1a68cacd700012ce9a7afe74d1503568
Valerio BIANCHI <valerio.bianchi@alten.it>
2023-09-14 15:55:58 +0200

    order fetching

---
+++ orders/src/test/setup.ts
@@ -0,0 +1,55 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+jest.mock("../nats-wrapper");
+
+let mongo: any;
+
+declare global {
+  var signin: () => string[];
+}
+
+beforeAll(async () => {
+  process.env.JWT_KEY = "asdfasdf";
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+  await mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: "test@test.com",
+  };
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  const session = { jwt: token };
+  const sessionJSON = JSON.stringify(session);
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+  return [`express:sess=${base64}`];
+  // const email = 'test@test.com';
+  // const password = 'password';
+  // const response = await request(app)
+  //     .post('/api/users/signup')
+  //     .send({
+  //         email, password
+  //     })
+  //     .expect(201)
+  // const cookie = response.get('Set-Cookie');
+  // return cookie;
+};
