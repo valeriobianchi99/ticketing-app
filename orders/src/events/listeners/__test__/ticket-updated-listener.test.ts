dce9c8cabb74cbca48f14dc7a1f5411ca1ddc7a8
Valerio BIANCHI <valerio.bianchi@alten.it>
2023-09-22 14:57:17 +0200

    testing listeners

---
+++ orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -0,0 +1,58 @@
+import { TicketUpdatedEvent } from "@sgtickets/common";
+import { natsWrapper } from "../../../nats-wrapper"
+import { TicketUpdatedListener } from "../ticket-updated-listener"
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../../models/ticket";
+
+const setup = async () => {
+    const listener = new TicketUpdatedListener(natsWrapper.client);
+    const ticket = Ticket.build({
+        id: new mongoose.Types.ObjectId().toHexString(),
+        title: 'concert',
+        price: 20
+    });
+    await ticket.save();
+    const data: TicketUpdatedEvent['data'] = {
+        version: ticket.version + 1,
+        id: ticket.id,
+        title: 'new concert',
+        price: 99,
+        userId: new mongoose.Types.ObjectId().toHexString()
+    };
+    // @ts-ignore
+    const msg: Message = {
+        ack: jest.fn()
+    };
+
+    return {
+        listener,
+        ticket,
+        data,
+        msg
+    };
+}
+
+it('finds, updates, and saves a ticket', async() => {
+    const { listener, data, ticket, msg } = await setup();
+    await listener.onMessage(data, msg);
+    const updatedTicket = await Ticket.findById(ticket.id);
+    expect(updatedTicket!.title).toEqual(data.title);
+    expect(updatedTicket!.price).toEqual(data.price);
+    expect(updatedTicket!.version).toEqual(data.version);
+})
+
+it('acks the message', async () => {
+    const { listener, data, msg } = await setup();
+    await listener.onMessage(data, msg);
+    expect(msg.ack).toHaveBeenCalled();
+})
+
+it('does not call ack if the event has a skipped version number', async () => {
+    const { listener, data, ticket, msg } = await setup();
+    data.version = 10;
+    try {
+        await listener.onMessage(data, msg);
+    } catch (err) {}
+    expect(msg.ack).not.toHaveBeenCalled();
+})
