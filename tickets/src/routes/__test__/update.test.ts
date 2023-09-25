14bd24903df359d425c903dc1ee6308417cccca3
Valerio BIANCHI <valerio.bianchi@alten.it>
2023-09-25 17:32:08 +0200

    rejecting updates on reserved tickets

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 import mongoose from 'mongoose';
 import { natsWrapper } from '../../__mocks__/nats-wrapper';
+import { Ticket } from '../../models/ticket';
 
 it('returns a 404 if the provided id does not exist', async() => {
     const customId = new mongoose.Types.ObjectId().toHexString()
@@ -104,4 +105,28 @@ it('publishes an event', async() => {
         })
         .expect(200);   
     expect(natsWrapper.client.publish).toHaveBeenCalled();
+})
+
+it('rejects updates if the ticket is reserved', async() => {
+    const cookie = global.signin();
+    const response = await request(app)
+        .post(`/api/tickets`)
+        .set('Cookie', cookie)
+        .send({
+            title: 'test',
+            price: 20
+        })
+        .expect(200);
+    const orderId = new mongoose.Types.ObjectId().toHexString();
+    const ticket = await Ticket.findById(response.body.id);
+    ticket?.set({ orderId });
+    await ticket!.save();
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: 'new-title',
+            price: 30
+        })
+        .expect(400); 
 })
