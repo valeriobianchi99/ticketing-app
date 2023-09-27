b63633748b9a59e7b155a1c729a711390f9de77a
Valerio BIANCHI <valerio.bianchi@alten.it>
2023-09-27 16:45:27 +0200

    enqueuing jobs

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -2,12 +2,16 @@ import { Listener, OrderCreatedEvent } from "@sgtickets/common";
 import { Subjects } from "@sgtickets/common/build/events/subjects";
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
+import { expirationQueue } from "../../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     subject: Subjects.OrderCreated = Subjects.OrderCreated;
     queueGroupName: string = queueGroupName;
 
     async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
-
+        await expirationQueue.add({
+            orderId: data.id
+        });
+        msg.ack();
     }
 }
--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,3 +1,4 @@
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
 import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
@@ -22,7 +23,7 @@ const start = async () => {
         });
         process.on('SIGINT', () => natsWrapper.client.close());
         process.on('SIGTERM', () => natsWrapper.client.close());
-
+        new OrderCreatedListener(natsWrapper.client).listen();
     } catch (error) {
         console.error(error);
     }
